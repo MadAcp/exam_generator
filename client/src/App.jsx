6e32776878f4d7f094547d0e4fed33bcf1bb8d5f
Avadhut Patil <avadhutcpatil@gmail.com>
2026-03-12 20:40:02 +0530

    Separate questions page created with datatable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage'
 import QuestionManagementPage from './pages/QuestionManagementPage'
 import PaperBuilderPage from './pages/PaperBuilderPage'
 import PapersListPage from './pages/PapersListPage'
+import Questions from './pages/Questions'
 
 function App() {
  
@@ -15,7 +16,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/questions" element={<QuestionManagementPage />} />
+          <Route path="/questions" element={<Questions />} />
+          {/* <Route path="/questions" element={<QuestionManagementPage />} /> */}
           <Route path="/builder" element={<PaperBuilderPage />} />
           <Route path="/papers" element={<PapersListPage />} />
         </Routes>
